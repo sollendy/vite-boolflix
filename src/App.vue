7b913bc58e84037b73e5bd3589552b94d81b2902
sollendy <edlannino@tutanota.com>
2023-03-31 12:34:25 +0200

    fix: problema con funzione cercaFilm risolto. Cercare di seguire lo schema

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <script>
+  import { store } from "./store";
+  import axios from "axios";
   import AppMain from './components/AppMain.vue';
   import AppSearch from './components/AppSearch.vue';
   export default {
+    data() {
+      return {
+        store,
+      }
+    },
     components: {
       AppSearch,
       AppMain,
     },
+    created() {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=fab6325a9f2906bf51076f0a3ebd6c67').then((res)=>{
+        this.store.film = res.data;
+        console.log(res)
+        // console.log(this.store.film)
+      });
+    },
+    methods: {
+      cercaFilm() {
+        let apiValore = this.store.filmCall
+        if(this.store.userInput != "") {
+          apiValore += `&query=${this.store.userInput}`;
+          console.log(apiValore)
+        }
+        axios.get(apiValore).then((response) => {
+        this.store.film = response.data;
+        console.log(response.data.results)
+        });
+      }
+    }
   }
 </script>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,7 @@
 <script>
     import {store} from "../store.js";
-    import axios from "axios";
-    import AppSearch from "./AppSearch.vue";
+    // import axios from "axios";
+    // import AppSearch from "./AppSearch.vue";
     export default {
         name: "AppMain",
         data() {
@@ -9,29 +9,9 @@
                 store,
             }
         },
-        components: {
-            AppSearch,
-        },
-        created() {
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=fab6325a9f2906bf51076f0a3ebd6c67').then((res)=>{
-                this.store.film = res.data;
-                console.log(res)
-                // console.log(this.store.film)
-            });
-        },
-        methods: {
-            cercaFilm() {
-                let apiValore = this.store.filmCall
-                if(this.store.userInput != "") {
-                    apiValore += `&query=${this.store.userInput}`;
-                    console.log(apiValore)
-                }
-                axios.get(apiValore).then((response) => {
-                    this.store.film = res.data;
-                    console.log(response.data.results)
-                });
-            }
-        }
+        // components: {
+        //     AppSearch,
+        // },
     }
 </script>
 
