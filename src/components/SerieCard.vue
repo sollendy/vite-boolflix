2ac516f1ea679140cb2b463fc959c59639a2ced6
sollendy <edlannino@tutanota.com>
2023-04-03 16:07:15 +0200

    fix: applicata bandierina anche a serie

--- src/components/SerieCard.vue
+++ src/components/SerieCard.vue
@@ -10,6 +10,38 @@
         props: {
             series: Object,
         },
+        computed: {
+            bandierina() {
+                switch (this.series.original_language) {
+                    case "en":
+                        return "gb";
+                    case "ja":
+                        return "jp";
+                    case "zh":
+                        return "cn";
+                    case "ko":
+                        return "kr";
+                    default:
+                        return this.series.original_language;
+                }
+                // let lingua = this.movie.original_language;
+                // if (lingua = "en") {
+                //     lingua = "gb";
+                //     console.log(lingua);
+                // }  else if (lingua = "ja") {
+                //          lingua = "jp"
+                // } else if (lingua = "ko") {
+                //      lingua = "kr"
+                // } else if (lingua = "zh") {
+                //      lingua = "cn"
+                // } else {
+                //      lingua = this.movie.original_language
+                // }
+                // return {
+                //     lingua
+                // }
+            }
+        }
     }
 </script>
 
@@ -20,7 +52,7 @@
             <div class="card-specs">
                 <div>Titolo: <strong>{{ series.name }}</strong></div>
                 <div>Titolo originale: <i>{{ series.original_name }}</i></div>
-                <div>Lingua Originale: <span :class="`fi fi-${series.original_language} fis`"></span></div>
+                <div>Lingua Originale: <span :class="`fi fi-${bandierina} fis`"></span></div>
                 <div>Punteggio: <i>{{ series.vote_average }}</i></div>
             </div>
         </div>
